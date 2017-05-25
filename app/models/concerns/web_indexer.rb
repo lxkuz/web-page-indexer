a469c36e81193046f6e4cbb87412852b2a778ba0
Alexey Kuznetsov <kuznetsov@mystand.ru>
2017-05-25 12:54:18 +0500

    fetch page content

--- app/models/concerns/web_indexer.rb
+++ app/models/concerns/web_indexer.rb
@@ -1,23 +1,31 @@
-# frozen_string_literal: true
 require 'active_support/concern'
 require 'uri'
 
 module WebIndexer
   extend ActiveSupport::Concern
-
+  
   included do
     before_save :fetch_content
+    cattr_accessor :__watch_tags  
 
+    def self.fetch_tags tags
+      self.__watch_tags = tags
+    end  
 
     private
 
     def fetch_content
-
-      response = Faraday.get URI(url).to_s 
-      body = response.body
+      begin 
+        html_string = Faraday.get(url).body
+        doc = Nokogiri::HTML html_string
+        res = {}
+        self.class.__watch_tags.each do |tag_name|
+          res[tag_name] = doc.xpath("//#{tag_name}").map(&:content)
+        end
+        self.content = res
+      rescue Exception => e
+        self.content = {error: e.to_s}
+      end
     end  
-
-
-
   end
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,7 +1,10 @@
 class Page < ApplicationRecord
   validates :url, presence: true
-
+  
   WATCH_TAGS = [:h1, :h2, :h3, :a]
+
+  include WebIndexer
+  fetch_tags WATCH_TAGS
   
   WATCH_TAGS.each do |tag|
     define_method "#{tag}_tags" do
@@ -9,5 +12,16 @@ class Page < ApplicationRecord
     end
   end
 
+  public
   
+  def url= url
+    write_attribute(:url, polish_url(url)) 
+  end
+
+  private
+
+  def polish_url url
+    return url if %r<^https?://>.match url
+    "http://#{url}"
+  end
 end
